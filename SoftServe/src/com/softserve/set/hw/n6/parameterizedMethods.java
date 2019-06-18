27ee6679b08bde79ac8af4bf6238322442cd1c0a
Taras Skobalo <taras.skobalo@gmail.com>
2019-06-18 22:16:34 +0300

    Collections-first task

---
+++ SoftServe/src/com/softserve/set/hw/n6/parameterizedMethods.java
@@ -0,0 +1,46 @@
+package com.softserve.set.hw.n6;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class parameterizedMethods {
+
+    public static Set<Integer> union(Set set1, Set set2) {
+        Set<Integer> setUnion = new HashSet<>();
+        setUnion.addAll(set2);
+        setUnion.addAll(set1);
+        return setUnion;
+    }
+
+
+    public static Set<Integer> intersect(Set set1, Set set2) {
+        Set<Integer> setIntersect = new HashSet<>();
+        setIntersect.addAll(set1);
+        setIntersect.retainAll(set2);
+        return setIntersect;
+    }
+
+    public static void main(String[] args) {
+        Random rd = new Random();
+
+        Set set1 = new HashSet<Integer>();
+        Set set2 = new HashSet<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            set1.add(rd.nextInt(30));
+            set2.add(rd.nextInt(15));
+        }
+        System.out.println(set1);
+        System.out.println(set2);
+
+        Set<Integer> setUnion = union(set1, set2);
+        System.out.println(setUnion);
+
+        Set<Integer> setIntersect = intersect(set1, set2);
+        System.out.println(setIntersect);
+
+
+
+    }
+}
